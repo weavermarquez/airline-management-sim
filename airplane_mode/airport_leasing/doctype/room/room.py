20f48745c613a06c58d668ae4801e4ac74efcdb3
kwalerie <weavermarquez0@gmail.com>
2024-08-28 00:33:22 +0000

    Update Room to use Action button

--- airplane_mode/airport_leasing/doctype/room/room.py
+++ airplane_mode/airport_leasing/doctype/room/room.py
@@ -2,6 +2,7 @@
 # For license information, please see license.txt
 
 import frappe
+import json
 from frappe.model.document import Document
 
 
@@ -14,85 +15,86 @@ class Room(Document):
 	if TYPE_CHECKING:
 		from frappe.types import DF
 
-		address: DF.Link | None
 		airport: DF.Link
-		airport_code: DF.ReadOnly | None
+		amended_from: DF.Link | None
 		area: DF.Int
 		capacity: DF.Int
-		item: DF.Link | None
 		operated_by: DF.Link | None
 		room_number: DF.Int
+		status: DF.Literal["Vacant", "Leased", "Under maintenance"]
 	# end: auto-generated types
 	pass
 
-	# NOTE This should no longer be necessary.
-	def create_related_location_entry(self):
-		airport_code = frappe.db.get_value('Airport', self.airport, 'code')
-		#airport_location = frappe.get_doc('Location', self.airport)
-
-		location = frappe.new_doc( 'Location', 
-			location_name = f'{airport_code}{self.room_number}',
-			parent_location = self.airport,
-			# TODO Create a fixture for Airport Location?
-		)
-
-		location.insert()
-		return location
+	# def create_related_item_entry(self):
+	# 	airport_code = frappe.db.get_value('Airport', self.airport, 'code')
+	# 	rental_rate = frappe.db.get_single_value('Airport Leasing Settings', 'default_rental_rate') 
 		
+	# 	item = frappe.new_doc( 'Item', 
+	# 		item_code = f'{airport_code}{self.room_number}',
+	# 		item_group = 'Real Estate Leasing',
+	# 		stock_uom = 'Week',
+	# 		is_stock_item = 0,
+	# 		is_purchase_item = 0,
+	# 		grant_commission = 0,
+	# 		sales_uom = 'Week',
+	# 		include_item_in_manufacturing = 0,
+	# 		standard_rate = rental_rate
+	# 	)
+	# 	# TODO Should I add Item Tax Template fields as well?
 
-	# NOTE This should no longer be necessary if Rooms are not listed as Assets.
-	def create_related_asset_entry(self):
-		airport_code = frappe.db.get_value('Airport', self.airport, 'code')
-		room_location = self.create_related_location_entry()
+	# 	# TODO Set the below if this room is somehow not for use.
+	# 	# is_sales_item = 0,
 
-		company = frappe.db.get_value('Lease', self.airport, 'leased_from')
+	# 	item.insert()
+	# 	return item
 
-		companies = frappe.get_list('Lease', 
-							  fields=['name', 'leased_from'], 
-							  filters={'leasing_of': self.name, 'docstatus': 1})
-							  # Add list-form filters for checking Dates of the Lease
+	def autoname(self):
+		self.name = f"{frappe.db.get_value('Airport', self.airport, 'code')}{self.room_number}"
 
-		if len(companies) == 0:
-			frappe.throw("This Room has no valid Leases")
-			# TODO Wait, what? Why does this need a valid Lease?
-		
-		asset = frappe.new_doc( 'Asset', 
-			company = companies[0].leased_from,
-			item_code = f'{airport_code}{self.room_number}',
-			asset_name = f'{airport_code}{self.room_number}',
-			location = room_location.name,
-			is_existing_asset = 1,
-			available_for_use_date = frappe.utils.today() , #today
-			gross_purchase_amount = 0,
-			asset_category = 'Airport Room'
-			# TODO Add Item Tax Template
-		)
 
-		asset.insert()
-		return asset
+# TODO Fix the fragility of this function with try / except.
+@frappe.whitelist()
+def create_item(doc: str):
+	"""
+	Creates a related Item entry for the Room.
 
-	def create_related_item_entry(self):
-		airport_code = frappe.db.get_value('Airport', self.airport, 'code')
-		
-		item = frappe.new_doc( 'Item', 
-			item_code = f'{airport_code}{self.room_number}',
-			item_group = 'Real Estate Leasing',
-			stock_uom = 'Week',
-			is_stock_item = 0,
-			is_purchase_item = 0,
-			grant_commission = 0,
-			sales_uom = 'Week',
-			include_item_in_manufacturing = 0
+	This method generates a new Item document with the following details:
+	- Item code: Combination of airport code and room number
+	- Item group: 'Real Estate Leasing'
+	- Stock UOM: 'Week'
+	- Sales UOM: 'Week'
+	- Is stock item: False
+	- Is purchase item: False
+	- Grant commission: False
+	- Include item in manufacturing: False
+	- Standard rate: Default rental rate from Airport Leasing Settings
 
-			#is_fixed_asset = 1,
-			#asset_category = 'Airport Room'
-			# TODO Add Item Tax Template
-		)
+	Returns:
+		frappe.Document: The newly created Item document
+	"""
+	doc_dict = json.loads(doc)
+	# Note that I can't seem to access fields of doc_dict with dot notation.
+	# Instead, I have to use doc_dict['field'].
 
-		item.insert()
-		return item
+	airport_code = frappe.db.get_value('Airport', doc_dict['airport'], 'code')
+	rental_rate = frappe.db.get_single_value('Airport Leasing Settings', 'default_rental_rate') 
 
-	def before_insert(self):
-		item = self.create_related_item_entry()
-		self.item = item.name
-		return True
+	item = frappe.new_doc('Item', 
+		item_code = f"{airport_code}{doc_dict['room_number']}",
+		item_group = 'Real Estate Leasing',
+		stock_uom = 'Week',
+		is_stock_item = 0,
+		is_purchase_item = 0,
+		grant_commission = 0,
+		sales_uom = 'Week',
+		include_item_in_manufacturing = 0,
+		standard_rate = rental_rate
+	)
+
+	item.insert()
+
+	frappe.msgprint(
+		msg='Item "%s" created as and associated to this Room.' % (item.name),
+		title='Room Item Creation',
+		indicator='green')
+	pass
