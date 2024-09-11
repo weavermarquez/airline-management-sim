89e271bfad4d42b61a5ed8207e97bac688a2dc6f
kwalerie <weavermarquez0@gmail.com>
2024-09-11 07:56:08 +0000

    Prepare Lease link Dashboard (inactive)

---
+++ airplane_mode/airport_leasing/doctype/lease/lease_dashboard.py
@@ -0,0 +1,18 @@
+from frappe import _
+
+
+# def get_data():
+#     pass
+    # return {
+    #     "fieldname": "lease",
+    #     "non_standard_fieldnames": {
+    #         "Sales Order": ["items", "item_code"],
+    #         "Sales Invoice": ["items", "item_code"],
+    #     },
+    #     "internal_links": {
+    #         "Item": "leasing_of",
+    #     },
+    #     "transactions": [
+    #         {"label": _("Lease Updates"), "items": [ "Item", "Sales Order", "Sales Invoice" ]},
+    #     ],
+    # }
