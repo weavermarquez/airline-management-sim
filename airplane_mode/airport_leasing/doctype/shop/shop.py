6e29a199268e0e1f4c5339cd711cd1f7f8b0b9b8
kwalerie <weavermarquez0@gmail.com>
2024-09-11 07:57:07 +0000

    Add Contact, Address to Shop form

--- airplane_mode/airport_leasing/doctype/shop/shop.py
+++ airplane_mode/airport_leasing/doctype/shop/shop.py
@@ -14,7 +14,21 @@ class Shop(Document):
 	if TYPE_CHECKING:
 		from frappe.types import DF
 
+		address_line1: DF.Data | None
+		address_line2: DF.Data | None
+		city: DF.Data | None
+		country: DF.Data | None
+		customer_primary_address: DF.Link | None
+		customer_primary_contact: DF.Link | None
+		customer_type: DF.ReadOnly | None
+		email_id: DF.ReadOnly | None
+		mobile_number: DF.Data | None
 		owned_by: DF.Link
+		pincode: DF.Data | None
+		primary_address: DF.Text | None
+		shop_name: DF.Data
 		shop_number: DF.Int
+		shop_type: DF.Literal["Restaurant", "Retail", "Services"]
+		state: DF.Data | None
 	# end: auto-generated types
 	pass
