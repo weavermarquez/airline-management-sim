21507154eda8ac9fff15ff7ba0d5a9471231b3e2
kwalerie <weavermarquez0@gmail.com>
2024-11-17 23:09:28 +0000

    fix: moved room retrieval to Shop

--- airplane_mode/airport_leasing/doctype/shop/shop.py
+++ airplane_mode/airport_leasing/doctype/shop/shop.py
@@ -1,34 +1,45 @@
 # Copyright (c) 2024, Weaver Marquez and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Shop(Document):
-	# begin: auto-generated types
-	# This code is auto-generated. Do not modify anything in this block.
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
 
-	from typing import TYPE_CHECKING
+    from typing import TYPE_CHECKING
 
-	if TYPE_CHECKING:
-		from frappe.types import DF
+    if TYPE_CHECKING:
+        from frappe.types import DF
 
-		address_line1: DF.Data | None
-		address_line2: DF.Data | None
-		city: DF.Data | None
-		country: DF.Data | None
-		customer_primary_address: DF.Link | None
-		customer_primary_contact: DF.Link | None
-		customer_type: DF.ReadOnly | None
-		email_id: DF.ReadOnly | None
-		mobile_number: DF.Data | None
-		owned_by: DF.Link
-		pincode: DF.Data | None
-		primary_address: DF.Text | None
-		shop_name: DF.Data
-		shop_number: DF.Int
-		shop_type: DF.Literal["Restaurant", "Retail", "Services"]
-		state: DF.Data | None
-	# end: auto-generated types
-	pass
+        address_line1: DF.Data | None
+        address_line2: DF.Data | None
+        city: DF.Data | None
+        country: DF.Data | None
+        customer_primary_address: DF.Link | None
+        customer_primary_contact: DF.Link | None
+        customer_type: DF.ReadOnly | None
+        email_id: DF.ReadOnly | None
+        mobile_number: DF.Data | None
+        owned_by: DF.Link
+        pincode: DF.Data | None
+        primary_address: DF.Text | None
+        shop_name: DF.Data
+        shop_number: DF.Int
+        shop_type: DF.Literal["Restaurant", "Retail", "Services"]
+        state: DF.Data | None
+    # end: auto-generated types
+    pass
+
+
+    @staticmethod
+    def rooms(shop: dict | Document) -> list[str]:
+        fields = ['leasing_of']
+        filters = {
+            'leased_to': shop.get('name'),
+            'docstatus': 1
+        }
+        return frappe.get_list('Lease', pluck='leasing_of', filters=filters)
+
--- airplane_mode/www/airport_shops.py
+++ airplane_mode/www/airport_shops.py
@@ -1,6 +1,5 @@
 import frappe
 from airplane_mode.airport_leasing.doctype.shop.shop import Shop
-from frappe.model.docstatus import DocStatus
 
 def get_context(context) -> dict:
     context.shops = shops()
@@ -8,11 +7,10 @@ def get_context(context) -> dict:
     context.get_desk_link = get_desk_link
     return context
 
-def shops():
-    """Call this function to grab a list of all Shops and their associated rooms."""
+def shops() -> list[dict]:
     shops: list[dict] = _all_shops()
     for shop in shops:
-        shop['rooms'] = leased_rooms(shop)
+        shop['rooms'] = Shop.rooms(shop)
     return shops
 
 def _all_shops() -> list[dict]:
@@ -22,20 +20,10 @@ def _all_shops() -> list[dict]:
     ]
     return frappe.get_all('Shop', fields=fields)
 
-def leased_rooms(shop: dict) -> list[str]:
-    fields = ['leasing_of']
-    filters = {
-        'leased_to': shop['name'],
-        'docstatus': 1
-    }
-    return frappe.get_list('Lease', pluck='leasing_of', filters=filters)
-
-
-def airport(room: str) -> str:
-    return frappe.get_cached_value('Room', room, 'airport')
-
-
-def get_desk_link(shop: dict, doctype: str = 'Shop'):
-    name = shop['name']
+def get_desk_link(shop: (dict | Shop), *, doctype='Shop') -> str:
+    name = shop.get('name')
     html = f'<a href="/app/Form/{doctype}/{name}" style="font-weight: bold;">Shop {name}</a>'
     return html
+
+def airport(room_name: str) -> str:
+    return frappe.get_cached_value('Room', room_name, 'airport')
