b6852b43b9b2067fb2f59c6fe357dd1efb43501e
kwalerie <weavermarquez0@gmail.com>
2024-11-17 22:52:19 +0000

    remove: /flight_agency extraneous portal page

--- airplane_mode/www/flight_agency.py
+++
@@ -1,8 +0,0 @@
-import frappe
-no_cache = 1
-
-def get_context(context):
-    context.title = "OwO Flight Agency"
-    pass
-    # context.color = frappe.form_dict.color if frappe.form_dict.color else 'black'
-    #Parameter name in /page?name="test" == frappe.form_dict.name
