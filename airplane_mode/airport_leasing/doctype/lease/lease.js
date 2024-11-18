920138fdbb74a9d5f84330db8f13a164b4279089
kwalerie <weavermarquez0@gmail.com>
2024-11-18 07:26:15 +0000

    feat: filter Room by status on lease form

--- airplane_mode/airport_leasing/doctype/lease/lease.js
+++ airplane_mode/airport_leasing/doctype/lease/lease.js
@@ -4,45 +4,56 @@
 frappe.ui.form.on("Lease", {
 	onload: function (frm) {
 		
-        frm.set_df_property('periods', 'cannot_add_rows', true);
-        frm.set_df_property('periods', 'cannot_delete_rows', true);
+		frm.set_df_property('periods', 'cannot_add_rows', true);
+		frm.set_df_property('periods', 'cannot_delete_rows', true);
 
-        frm.set_df_property('payments', 'cannot_add_rows', true);
-        frm.set_df_property('payments', 'cannot_delete_rows', true);
+		frm.set_df_property('payments', 'cannot_add_rows', true);
+		frm.set_df_property('payments', 'cannot_delete_rows', true);
 
-        let grid = frm.fields_dict["payments"].grid
-        let grid_button = grid.add_custom_button(
+		let grid = frm.fields_dict["payments"].grid
+		let grid_button = grid.add_custom_button(
 			__('Submit Payment'), 
 			() => { frm.events.create_lease_payment(frm) }
 		);
 
-        // grid_button.removeClass('btn-secondary')
-        //     .addClass('btn-primary')
-        //     .addClass('order-1');
+		const LEASE_DRAFT_ROOM_STATUS = (['Available', 'Reserved', 'Occupied', 'Maintenance'])
+		const LEASE_SUBMIT_ROOM_STATUS = (['Available', 'Reserved'])
 
-        // NOTE Applies a change to all custom buttons.
+		frm.set_query('leasing_of', () => {
+			return {
+				filters: [
+					['status', 'in', LEASE_SUBMIT_ROOM_STATUS]
+				]
+			}
+		});
+
+		// grid_button.removeClass('btn-secondary')
+		// .addClass('btn-primary')
+		// .addClass('order-1');
+
+		// NOTE Applies a change to all custom buttons.
 		// grid.buttons.find('.btn-custom')
-		// 	.removeClass('btn-default')
-		// 	.add_class('btn-primary');
+		// .removeClass('btn-default')
+		// .add_class('btn-primary');
 	},
 
-    create_lease_payment: function(frm) {
+	create_lease_payment: function(frm) {
 		frappe.prompt([
-    		{
-        		label: 'Payment in the amount of',
-        		fieldname: 'amount',
-        		fieldtype: 'Currency',
+			{
+				label: 'Payment in the amount of',
+				fieldname: 'amount',
+				fieldtype: 'Currency',
 				default: "500",
-   				non_negative: true,
-   				reqd: true,
-    		},
-    		{
-        		label: 'Reference Number',
-        		fieldname: 'reference_no',
-        		fieldtype: 'Data',
-   				reqd: true,
+				   non_negative: true,
+				   reqd: true,
+			},
+			{
+				label: 'Reference Number',
+				fieldname: 'reference_no',
+				fieldtype: 'Data',
+				reqd: true,
 				// default: frm.doc.leasing_of,
-    		},
+			},
 			], (values) => {
 				frm.call({ 
 					doc: frm.doc, 
